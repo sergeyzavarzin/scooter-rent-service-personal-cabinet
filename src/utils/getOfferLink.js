cf570e2ffd160763136cff3aa5c1eab71368f1d7
Sergey <tweek4000@yandex.ru>
2020-06-05 15:20:48 +0400

    Fix: add new offer links, fix styles

--- src/utils/getOfferLink.js
+++ src/utils/getOfferLink.js
@@ -5,11 +5,17 @@ export const getOfferLink = (category, discountCode) => {
 	if (category === 'courier' && discountCode.toUpperCase() === 'NEW_COURIER') {
 		return 'https://www.moysamokat.ru/oferta_delivery3300';
 	}
+	if (category === 'courier' && discountCode.toUpperCase() === 'COURIER2020') {
+		return 'https://www.moysamokat.ru/oferta_delivery3900';
+	}
 	if (category === 'courier') {
 		return 'https://moysamokat.ru/oferta_delivery2800';
 	}
 	if (discountCode.toUpperCase() === 'NEW_USER') {
 		return 'https://www.moysamokat.ru/oferta2900';
+	}
+	if (discountCode.toUpperCase() === 'USER2020') {
+		return 'https://www.moysamokat.ru/oferta3900';
 	}
 	return 'https://www.moysamokat.ru/oferta';
 };
