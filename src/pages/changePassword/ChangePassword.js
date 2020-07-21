de6e4f343933a07dd469582cd72e5f7fabcf7cb6
Maksim Isaev <mrredr@gmail.com>
2020-07-21 08:43:15 +0400

    Change user password

--- src/pages/changePassword/ChangePassword.js
+++ src/pages/changePassword/ChangePassword.js
@@ -1,93 +1,115 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
 import { Form, Input, Button, notification } from 'antd';
 
-import { change } from '../../globals/services/change';
+import { change, changeUserPassword } from '../../globals/services/change';
 import { passwordPattern } from '../../constants/passwordPattern';
 
 import './ChangePassword.scss';
 
-class ChangePassword extends React.Component {
-	state = {
-		hash: null,
-		isLoading: false,
-	};
+const ChangePassword = ({
+	store: {
+		userStore: { isUserLogged, email },
+	},
+	history,
+}) => {
+	const [hash, setHash]  = useState(null)
+	const [isLoading, setIsLoading] = useState(false);
 
-	componentDidMount() {
+	useEffect(() => {
 		const urlParams = new URLSearchParams(window.location.search);
 		const hash = urlParams.get('h');
 		if (hash && hash.length) {
-			this.setState({ hash });
-		} else {
+			setHash(hash);
+		} else if (!isUserLogged) {
 			window.location.href = '/';
 		}
-	}
+	}, [isUserLogged, setHash]);
 
-	onFinish = (values) => {
-		this.setState({ isLoading: true });
-		change(this.state.hash, values.password)
-			.then(() => {
-				notification.open({
-					message: 'Успешно.',
-					description: 'Пароль успешно изменен.',
-				});
-				setTimeout(() => {
-					this.props.history.push('/');
-				}, 3000);
-			})
-			.finally(() => this.setState({ isLoading: false }));
+	const onFinish = async (values) => {
+		setIsLoading(true);
+		try {
+			if (isUserLogged){
+				await changeUserPassword(values.password, values.oldPassword);
+			} else {
+				await change(hash, values.password);
+			}
+			notification.open({
+				message: 'Успешно.',
+				description: 'Пароль успешно изменен.',
+			});
+			setTimeout(() => {
+				history.push('/');
+			}, 3000);
+		} finally {
+			setIsLoading(false);
+		}
 	};
 
-	render() {
-		const { isLoading } = this.state;
-		return (
-			<div className='login-page'>
-				<div className='form'>
-					<h1>Сменить пароль</h1>
-					<Form name='login' onFinish={this.onFinish} className='form__form'>
+	return (
+		<div className='login-page'>
+			<div className='form'>
+				<h1>Сменить пароль</h1>
+				<Form name='login' onFinish={onFinish} className='form__form'>
+
+					{isUserLogged && 
 						<Form.Item
-							name='password'
+							name='oldPassword'
 							rules={[
-								{ required: true, message: 'Придумайте пароль' },
-								{ pattern: passwordPattern, message: 'Слабый пароль.' },
+								{ required: true, message: 'Введите свой пароль' },
 							]}
 							hasFeedback
 						>
-							<Input.Password placeholder='Придумайте пароль' />
+							<Input.Password placeholder='Введите текущий пароль' />
 						</Form.Item>
-						<Form.Item
-							name='retryPassword'
-							rules={[
-								{ required: true, message: 'Укажите пароль повторно' },
-								({ getFieldValue }) => ({
-									validator(rule, value) {
-										if (!value || getFieldValue('password') === value) {
-											return Promise.resolve();
-										}
-										return Promise.reject('Пароли не совпадают');
-									},
-								}),
-							]}
-							hasFeedback
+					}
+
+					<Form.Item
+						name='password'
+						rules={[
+							{ required: true, message: 'Придумайте пароль' },
+							{ pattern: passwordPattern, message: 'Слабый пароль.' },
+						]}
+						hasFeedback
+					>
+						<Input.Password placeholder='Придумайте пароль' />
+					</Form.Item>
+
+					<Form.Item
+						name='retryPassword'
+						rules={[
+							{ required: true, message: 'Укажите пароль повторно' },
+							({ getFieldValue }) => ({
+								validator(rule, value) {
+									if (!value || getFieldValue('password') === value) {
+										return Promise.resolve();
+									}
+									return Promise.reject('Пароли не совпадают');
+								},
+							}),
+						]}
+						hasFeedback
+					>
+						<Input.Password placeholder='Повторите пароль' />
+					</Form.Item>
+					
+					<Form.Item>
+						<Button
+							size='large'
+							type='primary'
+							htmlType='submit'
+							loading={isLoading}
+							className='form__button'
 						>
-							<Input.Password placeholder='Повторите пароль' />
-						</Form.Item>
-						<Form.Item>
-							<Button
-								size='large'
-								type='primary'
-								htmlType='submit'
-								loading={isLoading}
-								className='form__button'
-							>
-								Сменить пароль
-							</Button>
-						</Form.Item>
-					</Form>
-				</div>
+							Сменить пароль
+						</Button>
+					</Form.Item>
+
+				</Form>
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
-export default withRouter(ChangePassword);
+export default withRouter(inject('store')(observer(ChangePassword)));
